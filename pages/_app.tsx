658418fdf178bddaad1307a6d258406c7e6abbf1
Hanover <a@a.a>
2023-04-20 17:12:47 +0200

    some stuff for a preview

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,29 @@
 import '../styles/globals.css';
 import '@rainbow-me/rainbowkit/styles.css';
-import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import {connectorsForWallets, RainbowKitProvider} from '@rainbow-me/rainbowkit';
+import {metaMaskWallet, trustWallet, walletConnectWallet} from "@rainbow-me/rainbowkit/wallets";
 import type { AppProps } from 'next/app';
 import { configureChains, createClient, WagmiConfig } from 'wagmi';
-import { arbitrum, goerli, mainnet, optimism, polygon } from 'wagmi/chains';
+import { mainnet, bsc, bscTestnet } from 'wagmi/chains';
 import { publicProvider } from 'wagmi/providers/public';
+import {useState} from "react";
 
 const { chains, provider, webSocketProvider } = configureChains(
   [
     mainnet,
-    polygon,
-    optimism,
-    arbitrum,
-    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [goerli] : []),
+      {...bsc, name: 'Bsc Mainnet'},
+      {...bscTestnet, name: 'Bsc Testnet', iconUrl: 'https://umbria.network/assets/images/icon/bsclogo.png?v1'},
   ],
   [publicProvider()]
 );
 
-const { connectors } = getDefaultWallets({
-  appName: 'RainbowKit App',
-  projectId: 'YOUR_PROJECT_ID',
-  chains,
-});
+const connectors = connectorsForWallets([
+    {groupName: 'Recommended (we can change the title)', wallets: [
+        metaMaskWallet({chains}),
+            trustWallet({chains}),
+            walletConnectWallet({chains})
+        ]},
+])
 
 const wagmiClient = createClient({
   autoConnect: true,
@@ -31,10 +33,11 @@ const wagmiClient = createClient({
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
+    const [modalSize, setModalSize] = useState<'compact' | 'wide'>('wide')
   return (
     <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider chains={chains}>
-        <Component {...pageProps} />
+      <RainbowKitProvider chains={chains} showRecentTransactions={true} modalSize={modalSize}>
+        <Component {...pageProps} setModalSize={setModalSize} />
       </RainbowKitProvider>
     </WagmiConfig>
   );
